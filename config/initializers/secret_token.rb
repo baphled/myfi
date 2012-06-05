71b186aec9c8a556f271717eaca9b4213c40982d
Yomi Colledge <baphled@boodah.net>
2012-06-05 20:44:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFi::Application.config.secret_token = '162623022f20f7a236a4c90e48b310376cfdbe67562064a4c0e1c5e3981978274046546a25714cf441ae18c4273a5405287e80b84e6cd87a34cf9679b5193808'
