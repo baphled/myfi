b0c14cbd0642468403c20ea23c254d70660c1078
Yomi Colledge <baphled@boodah.net>
2012-06-09 22:17:45 +0100

    Display the monthly total

--- app/controllers/financial_breakdown_controller.rb
+++ app/controllers/financial_breakdown_controller.rb
@@ -1,4 +1,5 @@
 class FinancialBreakdownController < ApplicationController
+  before_filter :monthly_totals
   def index
     @income_breakdown = Income.monthly_breakdown
     @outgoing_breakdown = Outgoing.monthly_breakdown
